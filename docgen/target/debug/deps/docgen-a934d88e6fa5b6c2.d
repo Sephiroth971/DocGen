7181cecb9cf923c9ea3c913ed88ee06fb1dd61d5
sephiroth <avivies971@gmail.com>
2026-03-30 18:24:20 +0200

    init project

---
+++ docgen/target/debug/deps/docgen-a934d88e6fa5b6c2.d
@@ -0,0 +1,5 @@
+/home/useravivies/archdev/DocGen/docgen/target/debug/deps/docgen-a934d88e6fa5b6c2.d: src/main.rs
+
+/home/useravivies/archdev/DocGen/docgen/target/debug/deps/libdocgen-a934d88e6fa5b6c2.rmeta: src/main.rs
+
+src/main.rs:
