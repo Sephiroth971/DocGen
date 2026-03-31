d7868606172ddd33b590c978230cf2228b7d1a84
sephiroth <avivies971@gmail.com>
2026-03-31 10:00:22 +0200

    chore: remove target directory from repository

--- docgen/target/debug/deps/docgen-0da3ff8b77ca6c23.d
+++
@@ -1,7 +0,0 @@
-/home/useravivies/archdev/DocGen/docgen/target/debug/deps/docgen-0da3ff8b77ca6c23.d: src/main.rs src/cli.rs src/utils.rs
-
-/home/useravivies/archdev/DocGen/docgen/target/debug/deps/libdocgen-0da3ff8b77ca6c23.rmeta: src/main.rs src/cli.rs src/utils.rs
-
-src/main.rs:
-src/cli.rs:
-src/utils.rs:
